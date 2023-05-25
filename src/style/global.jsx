bbb13b59bfae2413bc105cba752479a90dd6b3b2
gabriel-victor933 <gabrielvictor933@usp.br>
2023-05-25 09:27:54 -0300

    adding global font family and size

--- src/style/global.jsx
+++ src/style/global.jsx
@@ -9,5 +9,27 @@ export const Global = createGlobalStyle`
     body {
         display: flex;
         justify-content: center;
+        font-family: "Source Sans Pro", "sans-seriff";
     }
+
+    h1 { 
+        font-size: 42px;
+    }
+    h2 { 
+        font-size: 32px;
+    }
+    h3 { 
+        font-size: 26px;
+    }
+    h4 { 
+        font-size: 22px;
+    }
+    p { 
+        font-size: 20px;
+    }
+
+    small {
+        font-size: 13px;
+    }
+
 `
