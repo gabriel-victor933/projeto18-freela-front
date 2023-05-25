647dda5f4f750e132b9810f220bcc879e44040b9
gabriel-victor933 <gabrielvictor933@usp.br>
2023-05-25 09:00:58 -0300

    feat: reset css and globla style

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { Reset } from './style/reset.jsx'
+import { Global } from './style/global.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Reset />
+    <Global />
     <App />
   </React.StrictMode>,
 )
---
+++ src/style/global.jsx
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from "styled-components";
+
+export const Global = createGlobalStyle`
+    
+    *{
+        box-sizing: border-box;
+    }
+
+    body {
+        display: flex;
+        justify-content: center;
+    }
+`
