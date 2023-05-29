2f7504bb531f8b941067bfaa3b2f8f74ac030aa6
gabriel-victor933 <gabrielvictor933@usp.br>
2023-05-29 09:46:55 -0300

    feat: adding feed posts

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -1,12 +1,26 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import Header from "../components/Header"
 import Search from "../components/Search"
 import PostButton from "../components/postButton"
+import axios from "axios"
+import Post from "../components/Post"
 
 export default function Feed(){
 
+    const [posts,setPosts] = useState([])
+
+    const config = {headers: 
+        {Authorization: `Bearer ${localStorage.getItem("token")}`}}
+
     useEffect(()=>{
-        //get feed route
+    
+        axios.get(`${import.meta.env.VITE_API_URL}/me/feed`,config)
+        .then((res)=>{
+            setPosts(res.data)
+        })
+        .catch((err)=>{
+            console.log(err)
+        })
 
     },[])
     
@@ -14,7 +28,7 @@ export default function Feed(){
         <>
         <Header />
         <Search />
-        <div>Feed</div>
+        {posts.map((post) => <Post key={post.id} post={post} />)}
         <PostButton />
         </>
     )
