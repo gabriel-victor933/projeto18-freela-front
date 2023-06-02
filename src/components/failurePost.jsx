6affa75fb0cc2e6be7b0fca53e7a75e3c7c2a789
gabriel-victor933 <gabrielvictor933@usp.br>
2023-06-02 13:31:34 -0300

    feat: adding style to post button

--- src/components/failurePost.jsx
+++ src/components/failurePost.jsx
@@ -1,11 +1,18 @@
+import { Forms } from "../style/forms"
+import { useNavigate } from "react-router-dom"
 
 export default function FailurePost({setPostState}){
+
+    const navigate = useNavigate()
 
     return (
         <div>
             <h3>Erro ao enviar post</h3>
-            <button onClick={() => setPostState(postState => ({ ...postState, sucess: false, finalized: false }))}>Tentar novamente</button>
-            <button>Voltar para o feed!</button>
+            <Forms>
+                <button onClick={() => setPostState(postState => ({ ...postState, sucess: false, finalized: false }))}>Tentar novamente</button>
+                <button onClick={()=>navigate("/feed")}>Voltar para o feed!</button>
+            </Forms>
+            
         </div>
     )
 }
--- src/components/sucessPost.jsx
+++ src/components/sucessPost.jsx
@@ -1,11 +1,13 @@
+import { Forms } from "../style/forms"
 
 export default function SucessPost({setPostState}){
 
     return (
         <div>
             <h3>post enviado</h3>
-            <button onClick={() => setPostState(postState => ({ ...postState, sucess: false, finalized: false }))}>Enviar outro!</button>
-            
+            <Forms>
+                <button onClick={() => setPostState(postState => ({ ...postState, sucess: false, finalized: false }))}>Enviar outro!</button>
+            </Forms>
         </div>
     )
 }
